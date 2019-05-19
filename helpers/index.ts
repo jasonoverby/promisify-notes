51cf51d02c323c256b2f43a1f0d4ac11c07d5f20
j0o00um <jason.overby@walmartlabs.com>
2019-05-19 06:49:45 -0700

    add helpers

---
+++ helpers/index.ts
@@ -0,0 +1,24 @@
+import chalk from 'chalk';
+
+const notANumber = (num: any): boolean => typeof num !== 'number' || Number.isNaN(num);
+const getWaitedSecsMsg = (num: number): string => `waited for ${num} seconds`;
+const getNotANumberMsg = (num1: number, num2: number): string => (
+  `"One or both of these arguments is not a number: ${num1}, ${num2}`);
+const getImprovedErrorMsg = (err: Error): string => (
+  chalk.red(err.stack.toString().split('\n').slice(0, 2).join('\n'))
+);
+
+const getRandomIntBetweenMinAndMax = (num1: number, num2: number): number => {
+  // ensures non-negative integers
+  const [abs1, abs2] = [num1, num2].map(Math.abs).map(Math.floor);
+  const [min, max] = [Math.min(abs1, abs2), Math.max(abs1, abs2)];
+  return Math.floor((Math.random() * (max - min)) + min);
+};
+
+export {
+  getImprovedErrorMsg,
+  getNotANumberMsg,
+  getRandomIntBetweenMinAndMax,
+  getWaitedSecsMsg,
+  notANumber,
+};
