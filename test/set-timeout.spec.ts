7f4d19baa1ab571cc0790d517feae50edae84250
j0o00um <jason.overby@walmartlabs.com>
2019-05-19 07:12:41 -0700

    add test for promisify setTimeout

---
+++ test/set-timeout.spec.ts
@@ -0,0 +1,14 @@
+import { promisify } from 'util';
+
+describe('promisified setTimeout', () => {
+  it('waits the specified number of ms', async () => {
+    const wait = promisify(setTimeout);
+    const before = Date.now();
+    await wait(2000);
+    const after = Date.now();
+    const timeWaited = after - before;
+    expect(timeWaited).toBeGreaterThan(2000);
+    expect(timeWaited).toBeLessThan(3000);
+    expect.assertions(2);
+  });
+});
